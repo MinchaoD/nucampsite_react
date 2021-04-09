8a6ddab49482ce7c8cec4df3e8ff0f1084dc3479
chaohg <chaohg@hotmail.com>
2021-04-08 20:30:03 -0700

    week2Assignment

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+
+class CampsiteInfo extends Component {
+
+
+
+    render(campsite){
+        if (campsite){
+            return (
+                <div className = "row">
+
+                </div>
+            );
+        }
+        return <div />;
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+export default CampsiteInfo;
