611ffc65e8420f6288115a72f25c3543a40f0795
chaohg <chaohg@hotmail.com>
2021-04-08 23:19:21 -0700

    add comments

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,27 +1,69 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            
 
+        }
+      
+    }
+    renderCampsite(campsite){
+        
+        return (
+            <div className = "col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                        
+                    </CardBody>
 
+                </Card>
+            </div>
+        )
 
-    render(campsite){
-        if (campsite){
+    }
+ 
+
+    renderComments(campsite){
+        if(campsite.comments){
             return (
-                <div className = "row">
+                <div className = "col-md-5 m-1">
+                    <h4>Comments</h4>
+                    
+                    {campsite.comments.map(comment => <div key={comment.id}> {comment.text} <br />-- {comment.author}{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} <br /><br /></div>)}
 
                 </div>
             );
         }
         return <div />;
-
-
     }
 
 
 
+    render(){
+        if (this.props.campsite){
+            return (
+                <div>
+                    <div className = "row">
+                        {this.renderCampsite(this.props.campsite)}
+                        
+                    </div>
+                    <div className = "row">
+                       
+                        {this.renderComments(this.props.campsite)}
+                    </div>
+                </div>  
+            );
+        } 
+        return <div />;
 
 
+    }
 
 
 }
