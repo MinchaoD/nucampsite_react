87c29c25df88b0f1097e7dc04dac33166f688729
chaohg <chaohg@hotmail.com>
2021-04-14 22:12:22 -0700

    React Router Parameters

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -40,7 +40,8 @@ function CampsiteInfo(props){
                 <div className="container">
                     <div className = "row">
                         {RenderCampsite(props.campsite)}
-                        {RenderComments(props.campsite.comments)}
+                        {RenderComments(props.comments)}
+                        
                     </div>
                   
                 </div>  
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -32,12 +33,25 @@ class Main extends Component {
                 />
             )
         }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+            
+            )
+        }
+
+
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
