deeed6ab000136aeee26e68bbb930e04446b40d1
chaohg <chaohg@hotmail.com>
2021-04-22 22:45:12 -0700

    add redux form to campsiteinfocomponent

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem,Modal, ModalHeader, ModalBody, Label, Col } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite(campsite){
         
@@ -20,21 +23,114 @@ function RenderCampsite(campsite){
         )
 
     }
- 
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+       
+        this.state = {
+            rating: '1',
+            author: '',
+            text: '',
+            touched: {
+                author: false,
+                text: false,
+            },
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    
+    handleSubmit(values) {
+        console.log('Current state is: ' + JSON.stringify(values));
+        alert('Current state is: ' + JSON.stringify(values));
+      
+    }
+
+    render() {
+        return (
+            <div>
+                <button outline onClick={this.toggleModal}><i className="fa fa-pencil fa-lg" /> Submit Comment </button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating" md={12}>Rating</Label>
+                                <Col md={12}>
+                                    <Control.select model=".rating" id="rating" name="rating"
+                                        className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </Col>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author" md={12}>Your Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                      <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text" md={12}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".text" id="text" name="text"
+                                        rows="6"
+                                        className="form-control"
+                                    />
+                                </Col>
+                            </div>
+                            <div className="form-group">
+                                <Col md={{size: 10, offset: 0}}>
+                                    <button className= "btn-primary" type="submit"> Submit </button>
+                                </Col>
+                            </div>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+}
+
 function RenderComments(comments){
         if(comments){
             return (
                 <div className = "col-md-5 m-1">
                     <h4>Comments</h4>
-                    
                     {comments.map(comment => <div key={comment.id}> {comment.text} <br />-- {comment.author}{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} <br /><br /></div>)}
-
+                    <CommentForm />
                 </div>
             );
         }
         return <div />;
     }
-
 
 function CampsiteInfo(props){
         if (props.campsite){
@@ -54,15 +150,13 @@ function CampsiteInfo(props){
                     <div className = "row">
                         {RenderCampsite(props.campsite)}
                         {RenderComments(props.comments)}
-                        
+                                 
                     </div>
                   
                 </div>  
             );
         } 
         return <div />;
-
-
     }
 
 
